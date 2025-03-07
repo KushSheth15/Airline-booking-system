70d037256feb3a341823ff943f5fb59fd415dcdd
KushSheth15 <kushsheth1511@gmail.com>
2025-03-07 12:35:12 +0530

    Added Initial Folders and Files

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { ServerConfig } = require('./config/index');
+
+const apiRoutes = require('./routes');
+
+const app = express();
+
+app.use('/api',apiRoutes);
+app.listen(ServerConfig.PORT , ()=>{
+    console.log(`🚀 Successfully started Server at ${ServerConfig.PORT}`);
+})
