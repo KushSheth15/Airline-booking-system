3349f9715d1a69793cff67a85724b21a4c1d262e
KushSheth15 <kushsheth1511@gmail.com>
2025-03-26 11:31:06 +0530

    Added Seats model in DB

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -1,15 +1,47 @@
+const {Sequelize} = require('sequelize');
 const CrudRepository = require('./crud-repository');
-const {Flight} = require('../models');
+const { Flight, Airplane , Airport} = require('../models');
 
 class FlightRepository extends CrudRepository {
-    constructor(){
+    constructor() {
         super(Flight);
     }
-    
-    async getAllFlights(filter,sort){
+
+    async getAllFlights(filter, sort) {
         const response = await Flight.findAll({
-            where:filter,
-            order:sort
+            where: filter,
+            order: sort,
+            include: [
+                {
+                    model: Airplane,
+                    required: true,
+                    as:'airplaneDetail'
+                },
+                {
+                    model: Airport,
+                    required: true,
+                    as:'departureAirport',
+                    on:{
+                        col1:Sequelize.where(Sequelize.col("Flight.departureAirportId"),"=",Sequelize.col("departureAirport.code"))
+                    },
+                    include:{
+                        model:City,
+                        required: true,
+                    }
+                },
+                {
+                    model: Airport,
+                    required: true,
+                    as:'arrivalAirport',
+                    on:{
+                        col1:Sequelize.where(Sequelize.col("Flight.arrivalAirportId"),"=",Sequelize.col("arrivalAirport.code"))
+                    },
+                    include:{
+                        model:City,
+                        required: true,
+                    }
+                }
+            ]
         });
         return response;
     }
