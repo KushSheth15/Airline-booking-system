05fd08cac1eaac4c3736356044c6d78e180a9bed
KushSheth15 <kushsheth1511@gmail.com>
2025-04-03 11:23:31 +0530

    Added booking API

--- src/repositories/booking-repository.js
+++ src/repositories/booking-repository.js
@@ -1,39 +1,58 @@
 const CrudRepository = require('./crud-repository');
-const {Booking} = require('../models/index');
+const { Booking } = require('../models/index');
+const { Enums } = require('../utils/common');
+const { Op } = require('sequelize');
+const { CANCELLED, BOOKED } = Enums.BOOKING_STATUS;
 
 class BookingRepository extends CrudRepository {
-    constructor(){
+    constructor() {
         super(Booking);
     }
-    
-    async createBooking(data,transaction){
-        const response = await Booking.create(data,{transaction:transaction});
+
+    async createBooking(data, transaction) {
+        const response = await Booking.create(data, { transaction: transaction });
         return response;
     }
 
-    async get(data,transaction){
-        const response = await Booking.findByPk(data,{transaction:transaction});
-        if(!response){
-            throw new Error('Not able to find resource',StatusCodes.NOT_FOUND);
+    async get(data, transaction) {
+        const response = await Booking.findByPk(data, { transaction: transaction });
+        if (!response) {
+            throw new Error('Not able to find resource', StatusCodes.NOT_FOUND);
         }
         return response;
     }
 
-    async update(id,data,transaction){
-        const response = await Booking.update(data,{
-            where:{
-                id:id
+    async update(id, data, transaction) {
+        const response = await Booking.update(data, {
+            where: {
+                id: id
             }
-        },{transaction:transaction});
+        }, { transaction: transaction });
         return response
     }
 
-    async cancelOldBookings(timestamp){
-        const response = await Booking.find({
-            where:{
-                createdAt:{
-                    [Op.gt]:timestamp
-                },
+    async cancelOldBookings(timestamp) {
+        const response = await Booking.update({
+            status: CANCELLED
+        }, {
+            where: {
+                [Op.and]: [
+                    {
+                        createdAt: {
+                            [Op.lt]: timestamp
+                        }
+                    },
+                    {
+                        status: {
+                            [Op.ne]: BOOKED
+                        }
+                    },
+                    {
+                        status: {
+                            [Op.ne]: CANCELLED
+                        }
+                    }
+                ]
             }
         });
         return response;
